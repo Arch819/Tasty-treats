12126cbd61efb3adaed45b8029f96004e98e3b06
Viacheslav <gabon.qwa@gmail.com>
2023-08-17 13:11:21 +0300

    settings repo

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//* header
+import './js/header/header';
+//* home
+  //hero
+import './js/home/hero/hero';
+  //?categories
+import './js/home/categories/categories';
+  //?popular recipes
+import './js/home/popular/popular';
+  //?recipes card
+import './js/home/recipe-cards/recipe-cards';
+//*favorite
+import './js/favorites/favorites';
+//*footer
+import './js/footer/footer'
