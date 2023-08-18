396ed3e512214875636d0fe3bef5ac82a9b55484
Igor2698 <128493043+Igor2698@users.noreply.github.com>
2023-08-18 13:55:41 +0300

    +

--- src/js/header/refs.js
+++
@@ -1,8 +0,0 @@
-export const refs = {
-    openModalButton: document.querySelector('.open-modal-button'),
-    modulWindow: document.querySelector('.mobile-menu'),
-    closeModalButton: document.querySelector('.close-modal-button'),
-    navigationLinks: document.querySelectorAll('.header-wrapper .header-navigation-list li a'),
-}
-
-
