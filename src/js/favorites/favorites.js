b93cc764371320914472d206654ef19b55a2b329
Viacheslav <gabon.qwa@gmail.com>
2023-08-18 17:23:17 +0300

    fix

--- src/js/favorites/favorites.js
+++ src/js/favorites/favorites.js
@@ -1,3 +1,18 @@
+// import { renderPageFavorits } from './renderFavorites';
+
+// const conRef = document.querySelector('.favorites__wrap');
+// console.log(conRef);
+
+// const favorites = () => {
+//   const keyOfLocalStorage = localStorage.getItem('setting');
+//   if (!keyOfLocalStorage) {
+//     renderPageFavorits(conRef); // Рендеримо сторінку якщо з LocalStorage нічого не отримано
+//     return;
+//   }
+// };
+
+// favorites();
+
 import { renderPageFavorits } from './renderFavorites';
 
 const conRef = document.querySelector('.favorites__wrap');
