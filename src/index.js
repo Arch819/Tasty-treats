585c7987db4bf782cfe435602fb6dc961dd300b1
Viacheslav <gabon.qwa@gmail.com>
2023-08-19 23:16:07 +0300

    add import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//?component
+import './js/components/components-index';
 //* header
 import './js/header/header';
 import './js/header/_switch-themes';
@@ -11,12 +13,11 @@ import './js/home/popular/popular';
 //?recipes card
 import './js/home/recipe-cards/recipe-cards';
 //*favorite
- import './js/favorites/favorites';
+import './js/favorites/favorites';
 //*footer
 import './js/footer/footer';
-//*modal 
+//*modal
 import './js/modal/modal-order/modal-order';
 import './js/modal/modal-rating/modal-rating';
 import './js/modal/modal-recipe/modal-recipe';
 import './js/modal/modal-createRecipe/createRecipeRender';
-
