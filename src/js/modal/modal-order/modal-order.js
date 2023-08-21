910eaf3f662f10000cce4a48fa4e242604a676fb
dmitrmedv <106028486+dmitrmedv@users.noreply.github.com>
2023-08-21 12:50:29 +0300

    add modal recipe

--- src/js/modal/modal-order/modal-order.js
+++ src/js/modal/modal-order/modal-order.js
@@ -1,135 +1,137 @@
-import Notiflix from "notiflix";
-const orderModal = () => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('.create-order-link'),
-    closeModalBtn: document.querySelector('.close'),
-    sendBtn: document.querySelector(".submit-btn"),
+// import Notiflix from 'notiflix';
+// const orderModal = () => {
+//   const refs = {
+//     openModalBtn: document.querySelectorAll('.create-order-link'),
+//     closeModalBtn: document.querySelector('.close'),
+//     sendBtn: document.querySelector('.submit-btn'),
 
-    modal: document.getElementById('modal'),
-    modalContent: document.querySelector('.order-modal'),
-    
-    buttonToSimulateClick: document.querySelector('#imitation'),
-  };
+//     modal: document.getElementById('modal'),
+//     modalContent: document.querySelector('.order-modal'),
 
-  // console.log(refs.openModalBtn);
-  // refs.buttonToSimulateClick.addEventListener('click', toggleModal);
-  refs.openModalBtn.forEach(button => { button.addEventListener('click', toggleModal); })
+//     buttonToSimulateClick: document.querySelector('#imitation'),
+//   };
 
+//   // console.log(refs.openModalBtn);
+//   // refs.buttonToSimulateClick.addEventListener('click', toggleModal);
+//   refs.openModalBtn.forEach(button => {
+//     button.addEventListener('click', toggleModal);
+//   });
 
-  refs.closeModalBtn.addEventListener('click', event => {
-    event.stopPropagation();
-    toggleModal();
-  });
+//   refs.closeModalBtn.addEventListener('click', event => {
+//     event.stopPropagation();
+//     toggleModal();
+//   });
 
+//   refs.modalContent.addEventListener('click', event => {
+//     event.stopPropagation();
+//   });
 
-  refs.modalContent.addEventListener('click', event => {
-    event.stopPropagation();
-  });
+//   refs.modal.addEventListener('click', event => {
+//     event.stopPropagation();
+//     toggleModal();
+//   });
 
+//   window.addEventListener('keydown', function (event) {
+//     if (event.key === 'Escape') {
+//       if (!refs.modal.classList.contains('is-hidden')) {
+//         toggleModal();
+//       }
+//     }
+//   });
 
-  refs.modal.addEventListener('click', event => {
-    event.stopPropagation();
-    toggleModal();
-  });
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     if (refs.modal.classList.contains('is-hidden')) {
+//       document.body.style.overflow = '';
+//     } else {
+//       document.body.style.overflow = 'hidden';
+//     }
+//   }
 
+//   const form = document.getElementById('order-form');
+//   const sendBtn = document.querySelector('.submit-btn');
+//   const inputs = form.querySelectorAll('.input-js');
 
-  window.addEventListener('keydown', function (event) {
-    if (event.key === 'Escape') {
-      if (!refs.modal.classList.contains('is-hidden')) {
-        toggleModal();
-      }
-    }
-  });
+//   inputs.forEach(function (input) {
+//     input.addEventListener('input', function () {
+//       validateInput(input);
+//       checkFormValidity();
+//     });
+//   });
+//   function validateInput(input) {
+//     if (input.checkValidity()) {
+//       input.classList.add('valid');
+//       input.classList.remove('invalid');
+//     } else {
+//       input.classList.add('invalid');
+//       input.classList.remove('valid');
+//     }
+//   }
+//   function checkFormValidity() {
+//     let isValid = true;
 
+//     inputs.forEach(function (input) {
+//       if (!input.checkValidity()) {
+//         isValid = false;
+//       }
+//     });
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    if (refs.modal.classList.contains('is-hidden')) {
-      document.body.style.overflow = '';
-    } else {
-      document.body.style.overflow = 'hidden';
-    }
-  }
+//     if (isValid) {
+//       sendBtn.removeAttribute('disabled');
+//     } else {
+//       sendBtn.setAttribute('disabled', 'disabled');
+//     }
+//   }
 
-  const form = document.getElementById("order-form");
-  const sendBtn = document.querySelector(".submit-btn");
-  const inputs = form.querySelectorAll(".input-js");
+//   form.addEventListener('submit', function (event) {
+//     event.preventDefault();
 
-  inputs.forEach(function (input) {
-    input.addEventListener("input", function () {
-      validateInput(input);
-      checkFormValidity();
-    });
-  });
-  function validateInput(input) {
-    if (input.checkValidity()) {
-      input.classList.add("valid");
-      input.classList.remove("invalid");
-    } else {
-      input.classList.add("invalid");
-      input.classList.remove("valid");
-    }
-  }
-  function checkFormValidity() {
-    let isValid = true;
+//     if (form.checkValidity()) {
+//       const formData = {
+//         name: form.name.value.trim(),
+//         phone: form.phone.value.trim(),
+//         email: form.email.value.trim(),
+//         comment: form.comment.value.trim(),
+//       };
+//       if (
+//         formData.name === '' ||
+//         formData.phone === '' ||
+//         formData.email === ''
+//       ) {
+//         Notiflix.Report.failure('Please fill out all required fields.');
+//         return;
+//       }
+//       // Збереження даних у localStorage
+//       saveFormDataToLocalStorage(formData);
+//       console.log(`https://tasty-treats-backend.p.goit.global/api/orders/add&{
+//             name: ${formData.name},
+//             phone: ${formData.phone},
+//             email: ${formData.email},
+//             comment:${formData.comment},
+//         }`);
 
-    inputs.forEach(function (input) {
-      if (!input.checkValidity()) {
-        isValid = false;
-      }
-    });
+//       Notiflix.Report.success(
+//         'Your order has successfully been sent. Thank you!',
+//         'OK'
+//       );
 
-    if (isValid) {
-      sendBtn.removeAttribute("disabled");
-    } else {
-      sendBtn.setAttribute("disabled", "disabled");
-    }
-  }
+//       form.reset();
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
+//       setTimeout(() => {
+//         toggleModal();
+//       }, 1000);
+//     } else {
+//       Notiflix.Report.failure('Please enter valid data.');
+//     }
+//   });
 
-    if (form.checkValidity()) {
-      const formData = {
-        name: form.name.value.trim(),
-        phone: form.phone.value.trim(),
-        email: form.email.value.trim(),
-        comment: form.comment.value.trim()
-            
-      };
-      if (formData.name === "" || formData.phone === "" || formData.email === "") {
-        Notiflix.Report.failure("Please fill out all required fields.");
-        return;
-      }
-      // Збереження даних у localStorage
-      saveFormDataToLocalStorage(formData);
-      console.log(`https://tasty-treats-backend.p.goit.global/api/orders/add&{
-            name: ${formData.name},
-            phone: ${formData.phone},
-            email: ${formData.email},
-            comment:${formData.comment},
-        }`);
-        
-      Notiflix.Report.success("Your order has successfully been sent. Thank you!", "OK");
+//   // Збереження даних у localStorage
+//   function saveFormDataToLocalStorage(formData) {
+//     const storedData = JSON.parse(localStorage.getItem('storedData')) || [];
+//     storedData.push(formData);
+//     localStorage.setItem('storedData', JSON.stringify(storedData));
+//   }
+// };
 
-      form.reset();
-
-      setTimeout(() => {
-        toggleModal();
-      }, 1000);
-      
-    } else {
-      Notiflix.Report.failure("Please enter valid data.");
-    }
-  });
-
-  // Збереження даних у localStorage
-  function saveFormDataToLocalStorage(formData) {
-    const storedData = JSON.parse(localStorage.getItem("storedData")) || [];
-    storedData.push(formData);
-    localStorage.setItem("storedData", JSON.stringify(storedData));
-  }
-}
-
-orderModal();
-export default orderModal;
+// orderModal();
+// export default orderModal;
---
+++ src/js/modal/modal-recipe/modal-recipeApi.js
@@ -0,0 +1,10 @@
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/recipes/';
+
+export default function fetchRecipe(id) {
+  return fetch(`${BASE_URL}${id}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
