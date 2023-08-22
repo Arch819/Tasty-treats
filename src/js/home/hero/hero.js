f1b51365d8e5359d2456c1f0bcb88113ec692ebb
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-08-22 10:26:25 -0400

    thirteenth commit
    close button sticky

--- src/js/home/hero/hero.js
+++ src/js/home/hero/hero.js
@@ -1,34 +1,34 @@
-import Swiper from 'swiper';
-import 'swiper/swiper-bundle.min.css';
-import { fetchEvent } from "./heroApi";
-import { eventRender } from './heroRender';
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.min.css';
+// import { fetchEvent } from "./heroApi";
+// import { eventRender } from './heroRender';
 
-const swiperEvent = document.querySelector(".swiper-wrapper");
+// const swiperEvent = document.querySelector(".swiper-wrapper");
 
-fetchEvent().then(data => {
-    swiperEvent.insertAdjacentHTML('beforeend', eventRender(data));
-    const swiper = new Swiper('.swiper', {
-      // modules: [Pagination, Navigation, Autoplay], 
-      allowSlideNext: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type: 'bullets',
-        dynamicBullets: true,
-      },
-   spaceBetween: 10,
-      autoplay: {
-        delay: 3000,
-      },
-//  к-сть показаних слайдів на сторінку
-  slidesPerView: 1,
-  // к-сть слайдів які пролистуються
-  slidesPerGroup: 1,
-      speed: 800,
-      loop: true,
-      mousewheel: {
-    invert: true,
-  },
-    });
-  }
-);
+// fetchEvent().then(data => {
+//     swiperEvent.insertAdjacentHTML('beforeend', eventRender(data));
+//     const swiper = new Swiper('.swiper', {
+//       // modules: [Pagination, Navigation, Autoplay], 
+//       allowSlideNext: true,
+//       pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         type: 'bullets',
+//         dynamicBullets: true,
+//       },
+//    spaceBetween: 10,
+//       autoplay: {
+//         delay: 3000,
+//       },
+// //  к-сть показаних слайдів на сторінку
+//   slidesPerView: 1,
+//   // к-сть слайдів які пролистуються
+//   slidesPerGroup: 1,
+//       speed: 800,
+//       loop: true,
+//       mousewheel: {
+//     invert: true,
+//   },
+//     });
+//   }
+// );
