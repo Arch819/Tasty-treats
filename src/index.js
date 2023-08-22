7e1f687fb654973058b893d8af616a2466baa2b9
dmitrmedv <dmitrmedv@gmail.com>
2023-08-22 08:18:26 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 //?component
 import './js/components/components-index';
 //* header
- import './js/header/header';
- import './js/header/_switch-themes';
+import './js/header/header';
+import './js/header/_switch-themes';
 //* home
 //hero
- import './js/home/hero/hero';
+import './js/home/hero/hero';
 //?categories
- import './js/home/categories/categories';
+import './js/home/categories/categories';
 //?popular recipes
- import './js/home/popular/popular';
+import './js/home/popular/popular';
 //?recipes card
- import './js/home/recipe-cards/recipe-cards';
+import './js/home/recipe-cards/recipe-cards';
 //*favorite
-import './js/favorites/favorites';
+// import './js/favorites/favorites';
 //*footer
- import './js/footer/footer';
+import './js/footer/footer';
 //*modal
- import './js/modal/modal-order/modal-order';
- import './js/modal/modal-rating/modal-rating';
- import './js/modal/modal-recipe/modal-recipe';
- import './js/modal/modal-createRecipe/createRecipe';
+import './js/modal/modal-order/modal-order';
+import './js/modal/modal-rating/modal-rating';
+import './js/modal/modal-recipe/modal-recipe';
+import './js/modal/modal-createRecipe/createRecipe';
