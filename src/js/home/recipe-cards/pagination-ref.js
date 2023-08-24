5e34a69276d2239be687c5b4cda91abc73693f1d
IgorMushk <igor.mushketik@gmail.com>
2023-08-24 19:14:41 +0300

    export refs pagination

---
+++ src/js/home/recipe-cards/pagination-ref.js
@@ -0,0 +1,12 @@
+export const refs = {
+  backToFirstPage: document.querySelector('#pag-btn-start'),
+  pageOneBtn: document.querySelector('#pag-btn-1'),
+  pageTwoBtn: document.querySelector('#pag-btn-2'),
+  pageThreeBtn: document.querySelector('#pag-btn-3'),
+  lastPageBtn: document.querySelector('#pag-btn-last'),
+  nextPagePagBtn: document.querySelector('#pag-btn-next'),
+  buttonNumered: document.querySelectorAll('.pag-btn-number'),
+  previousPageButton: document.querySelector('#pag-btn-prev'),
+  btnWithDotsRight: document.querySelector('#pag-btn-dots-right'),
+  btnWithDotsLeft: document.querySelector('#pag-btn-dots-left'),
+};
