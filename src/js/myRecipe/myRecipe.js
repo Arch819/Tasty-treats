845339c08815882343b5dd60d1b25343d2550edc
Viacheslav <116117866+Arch819@users.noreply.github.com>
2023-09-20 15:50:35 +0300

    Update myRecipe.js

--- src/js/myRecipe/myRecipe.js
+++ src/js/myRecipe/myRecipe.js
@@ -1,6 +1,9 @@
 import '../components/components-index';
 import '../header/header';
 import '../header/_switch-themes';
+import './js/footer/footer';
+//*modal
+import './js/modal/modal-createRecipe/createRecipe';
 import { fetchMyRecipe } from './myRecipeFetch';
 import { renderRecipeCard } from './myRecipeRender';
 
